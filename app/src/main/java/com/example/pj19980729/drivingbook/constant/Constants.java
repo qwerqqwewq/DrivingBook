7ccbc5142a28584f8ecb8728cfeb8491bc41e20b
qwerqqwewq <2725543850@qq.com>
2019-07-11 14:50:36 +0800

    123

--- app/src/main/java/com/example/pj19980729/drivingbook/constant/Constants.java
+++ app/src/main/java/com/example/pj19980729/drivingbook/constant/Constants.java
@@ -1,5 +1,5 @@
 package com.example.pj19980729.drivingbook.constant;
 
 public final class Constants {
-    static public String context = "http:10.13.31.160/drive";
+    static public String context = "http:10.13.31.209/drive";
 }
